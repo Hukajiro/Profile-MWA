b7c7f86da68b51e5f2845bf92100c859ee7a6fe4
M. Wildan Alif <wildanalif290@gmail.com>
2024-09-08 19:45:53 +0700

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,16 +1,19 @@
 public class Profile {
     String Nama;
     String NIM;
+    String Hobi:
 
-    Profile(String inputNama,String inputNIM){
+    Profile(String inputNama,String inputNIM,String inputHobi){
         Nama = inputNama;
         NIM = inputNIM;
+        Hobi = inputHobi;
 
         System.out.println("Nama = " + Nama);
         System.out.println("NIM  = " + NIM);
+        System.out.println("Hobi = " + Hobi);
     }
     public static void main(String[] args) {
-        Profile profile1 = new Profile("M. WILDAN ALIF", "12350113259");
+        Profile profile1 = new Profile("M. WILDAN ALIF", "12350113259", "BERMAIN VOLI");
 
     }
 }
